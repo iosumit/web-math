bc488a4cd7b210e27bf89a05a66a8b333b0febe5
Sumit Kumar <sk4390789@gmail.com>
2021-11-20 18:27:08 +0530

    objects in circular path

--- main.js
+++ main.js
@@ -6,31 +6,23 @@ window.onload= function () {
 
     let centerY = height * .5,
         centerX = width * .5,
-        xRadius = 200,
-        yRadius = 400,
-        xspeed = 0.1,
-        yspeed = 0.131,
-        xangle = 0, 
-        yangle = 0, 
+        radius = 200,
         angle = 0, 
+        speed = 0.01,
+        numObject = 20,
+        slice = Math.PI * 2 / numObject,   // Radian between 2 Obj
         x, y;
 
-    render();
+    
 
-    function render() {
-        context.clearRect(0, 0, width, height); // Clearing the canvas
-        
-        x = centerX + Math.cos(xangle) * xRadius; 
-        y = centerY + Math.sin(yangle) * yRadius; 
+    for (let i = 0; i < numObject; i++) {
+        angle = i * slice;   // Calculating the radian of obj
+        x = centerX + Math.cos(angle) * radius;
+        y = centerY + Math.sin(angle) * radius;
 
         context.beginPath();
         context.arc(x, y, 10, 0, Math.PI * 2, false);
         context.fill();
-
-        xangle += xspeed; 
-        yangle += yspeed; 
-        console.log(x, y);
-
-        requestAnimationFrame(render);
-    } 
+        console.log(angle);
+    }
 }
