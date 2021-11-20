1acbb8d597fb22df9158716fe64c805b925f8257
Sumit Kumar <sk4390789@gmail.com>
2021-11-20 17:59:17 +0530

    lissajous curve

--- main.js
+++ main.js
@@ -8,23 +8,27 @@ window.onload= function () {
         centerX = width * .5,
         xRadius = 200,
         yRadius = 400,
-        speed = 0.005,
+        xspeed = 0.1,
+        yspeed = 0.131,
+        xangle = 0, 
+        yangle = 0, 
         angle = 0, 
         x, y;
 
     render();
 
     function render() {
-        // context.clearRect(0, 0, width, height); // Clearing the canvas
+        context.clearRect(0, 0, width, height); // Clearing the canvas
         
-        x = centerX + Math.cos(angle) * xRadius; 
-        y = centerY + Math.sin(angle) * yRadius; 
+        x = centerX + Math.cos(xangle) * xRadius; 
+        y = centerY + Math.sin(yangle) * yRadius; 
 
         context.beginPath();
         context.arc(x, y, 10, 0, Math.PI * 2, false);
         context.fill();
 
-        angle += speed; 
+        xangle += xspeed; 
+        yangle += yspeed; 
         console.log(x, y);
 
         requestAnimationFrame(render);
