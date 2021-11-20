0c5526508cc3fe3e86320232222b9b4b42aa16f2
Sumit Kumar <sk4390789@gmail.com>
2021-11-20 16:41:27 +0530

    circle growingand shrinking

--- main.js
+++ main.js
@@ -4,28 +4,26 @@ window.onload= function () {
     width = canvas.width = window.innerWidth,
     height = canvas.height = window.innerHeight;
 
-    // context.translate(0, height / 2);
-    // context.scale(1, -1);
-
     let centerY = height * .5,
         centerX = width * .5,
-        offset = height * .4,
+        baseRadius = 150,
+        offset = 50,
         speed = 0.1,
         angle = 0;
 
     render();
 
     function render() {
-        var y = centerY + Math.sin(angle) * offset; //( y is repeatitive value up and down)
+        var radius = baseRadius + Math.sin(angle) * offset; //( y is repeatitive value up and down)
 
         context.clearRect(0, 0, width, height); // Clearing the canvas
         context.beginPath();
 
-        context.arc(centerX, y, 50,  0, Math.PI * 2, false);
+        context.arc(centerX, centerY, radius,  0, Math.PI * 2, false);
         context.fill();
 
         angle += speed;
-        console.log(y);
+        console.log(radius);
 
         requestAnimationFrame(render);
     } 
